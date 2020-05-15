72d5b5945fd5f62a9944f66d9c91f96a19ee4e1e
Albert Magyar <albert.magyar@gmail.com>
2020-05-14 22:38:16 -0700

    Make Utils.expandRef properly return intermediate expressions

--- src/main/scala/firrtl/Utils.scala
+++ src/main/scala/firrtl/Utils.scala
@@ -287,9 +287,9 @@ object Utils extends LazyLogging {
     case ex => ex.tpe match {
       case (_: GroundType) => Seq(ex)
       case (t: BundleType) => (t.fields foldLeft Seq[Expression](ex))((exps, f) =>
-        exps ++ create_exps(WSubField(ex, f.name, f.tpe,times(flow(ex), f.flip))))
+        exps ++ expandRef(WSubField(ex, f.name, f.tpe,times(flow(ex), f.flip))))
       case (t: VectorType) => (0 until t.size foldLeft Seq[Expression](ex))((exps, i) =>
-        exps ++ create_exps(WSubIndex(ex, i, t.tpe,flow(ex))))
+        exps ++ expandRef(WSubIndex(ex, i, t.tpe,flow(ex))))
     }
   }
   def toTarget(main: String, module: String)(expression: Expression): ReferenceTarget = {
